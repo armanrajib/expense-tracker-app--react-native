8a6148d9f44793ecd9eec8b7feca167a0ef52e0d
Arman Rajib <ahrajib6789@gmail.com>
2024-12-06 01:48:13 +0600

    Add Navigation (with React Navigation) & Configure Navigation

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import AllExpenses from "./screens/AllExpenses";
+import RecentExpenses from "./screens/RecentExpenses";
+import ManageExpense from "./screens/ManageExpense";
+
+const Stack = createNativeStackNavigator();
+const BottomTab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="ExpensesOverview" component={ExpensesOverview} />
+          <Stack.Screen name="ManageExpense" component={ManageExpense} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+function ExpensesOverview() {
+  return (
+    <BottomTab.Navigator>
+      <BottomTab.Screen name="AllExpenses" component={AllExpenses} />
+      <BottomTab.Screen name="RecentExpenses" component={RecentExpenses} />
+    </BottomTab.Navigator>
+  );
+}
